d3c71b5069d6657fb1ca152fa449615a84693c67
Wiquar2110 <eng23ds0105@dsu.edu.in>
2024-12-21 22:46:28 +0530

    Add files via upload

---
+++ alphabets.java
@@ -0,0 +1,13 @@
+public class alphabets {
+    public static void main(String[] args) {
+        System.out.println("uppercase alphabets:");
+        for(int i=65;i<=90;i++){
+            System.out.println((char)i+ "  ");
+        }
+        System.out.println("lowercase alphabets:");
+        for(int i=97;i<=122;i++){
+            System.out.println((char)i+ " ");
+        }
+    }
+    
+}
